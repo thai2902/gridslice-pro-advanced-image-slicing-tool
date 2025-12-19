51869bc5ccb70c26571cead20b73f1e1489b5122
Vibesdk <vibesdk-bot@cloudflare.com>
2025-12-19 03:45:20 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,11 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add jszip@^3.10.1",
+    "bun add file-saver@^2.0.5",
+    "bun add react-dropzone@^14.2.3"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
